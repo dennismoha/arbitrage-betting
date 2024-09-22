8c88ba1fcca8705307769cb07f9d339f8e753493
dennismoha <dennismoha2@gmail.com>
2024-09-22 11:14:30 +0300

    rebased off feature/redux-state , added types for 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,16 +1,21 @@
 import { useRouteError } from "react-router-dom";
 
+interface MyError {
+  statusText?: string;
+  message?: string;
+  // Add other fields as necessary
+}
 
 export default function NotFound() {
-const error = useRouteError();
-  console.error(error);
+const error = useRouteError() as MyError;
+
 
   return (
     <div id="error-page">
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
       <p>
-        {/* <i>{error.statusText || error.message}</i> */}
+        <i>{error.statusText || error.message}</i>
       </p>
     </div>
   );
